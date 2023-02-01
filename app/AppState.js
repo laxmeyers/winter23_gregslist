f28fa177000d1edd3b423bc0cbf76461c293d467
Andrew Meyers <laxmeyers@gmail.com>
2023-02-01 14:59:38 -0700

    house array made

--- app/AppState.js
+++ app/AppState.js
@@ -1,4 +1,5 @@
 import { Car } from "./Models/Car.js"
+import { House } from "./Models/House.js"
 import { Value } from "./Models/Value.js"
 import { EventEmitter } from "./Utils/EventEmitter.js"
 import { isValidProp } from "./Utils/isValidProp.js"
@@ -12,6 +13,24 @@ class AppState extends EventEmitter {
   cars = loadState('cars', [Car])
   /** @type {import('./Models/Car').Car} */
   car = null
+  
+  /** @type {import('./Models/House').House[]} */
+  houses = [
+    new House ({
+      year: '1974',
+      name: 'Mid-Century Modern Single Family Home',
+      bedrooms: 3,
+      bathrooms: 2,
+      sqtf : 1400,
+      price: 375600,
+      description: 'old nice house',
+      imgUrl: 'https://images.unsplash.com/photo-1568605114967-8130f3a36994?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    })
+    
+  ]
+  
+  /** @type {import('./Models/House').House} */
+  house = null
 }
 
 export const appState = new Proxy(new AppState(), {
--- app/Models/House.js
+++ app/Models/House.js
@@ -1,3 +1,16 @@
+import { generateId } from "../Utils/generateId"
 
 
-export class House { }
+export class House {
+    constructor(data) {
+        this.id = data.id || generateId()
+        this.year = data.year
+        this.name = data.name
+        this.bedrooms = data.bedrooms
+        this.bathrooms = data.bathrooms
+        this.sqft = data.sqft
+        this.price = data.price
+        this.description = data.description
+        this.imgUrl = data.imgUrl
+    }
+}
